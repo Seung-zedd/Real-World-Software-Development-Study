bd3e95c100ba39cb2da35b9cff5622f2b6972a42
seung-je <csz0209@gmail.com>
2025-02-21 23:21:02 +0900

    feat: add BankStatementCSVParser and BankTransaction

---
+++ OOP/src/main/java/com/real_software/chapter_02/BankStatementCSVParser.java
@@ -0,0 +1,33 @@
+package com.real_software.chapter_02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+//* 파싱 로직을 추출해 한 클래스로 만듦
+public class BankStatementCSVParser {
+
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(final String line) {
+        // 콤마로 구분
+        final String[] columns = line.split(",");
+
+        // 구분한 열들을 파싱해서 date, amount, description 변수에 저장
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]); // 금액 추출 및 double로 파싱
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    //*
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for (String line : lines) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
+    }
+}
--- OOP/src/main/java/com/real_software/chapter_02/BankTransactionAnalyzerSimple.java
+++ OOP/src/main/java/com/real_software/chapter_02/BankTransactionAnalyzerSimple.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 //* 모든 거래 내역의 합 계산하기
@@ -11,18 +14,39 @@ public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main(String[] args) throws IOException {
+        //* 예제 2-1
         // 파일 시스템의 경로를 응용프로그램의 명령줄 인수로 전달해 로딩
-        final Path path = Paths.get(RESOURCES + args[0]);
+        Path path = Paths.get(RESOURCES + args[0]);
         // CSV파일의 행 목록을 반환
         List<String> lines = Files.readAllLines(path);
         double total = 0d;
 
         for (final String line : lines) {
             // CSV파일의 구분자는 콤마라고 가정
-            final String[] columns = line.split(",");
-            final double amount = Double.parseDouble(columns[1]); // 1 col에 가격이 있음
+            final String[] columns = line.split(","); // 콤마로 열 분리
+            final double amount = Double.parseDouble(columns[1]); // 금액 추출 및 double로 파싱
             total += amount;
         }
         System.out.println("The total for all transactions is " + total);
+
+        //* 예제 2-2: 1월 입출금 내역 합계 계산하기
+        // 파일 시스템의 경로를 응용프로그램의 명령줄 인수로 전달해 로딩
+        path = Paths.get(RESOURCES + args[0]);
+        // CSV파일의 행 목록을 반환
+        lines = Files.readAllLines(path);
+        total = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        for (final String line : lines) {
+            // CSV파일의 구분자는 콤마라고 가정
+            final String[] columns = line.split(","); // 콤마로 열 분리
+            LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+            if (date.getMonth() == Month.JANUARY) {
+                final double amount = Double.parseDouble(columns[1]); // 금액 추출 및 double로 파싱
+                total += amount;
+            }
+        }
+        System.out.println("The total for all transactions in January is " + total);
     }
+
 }
