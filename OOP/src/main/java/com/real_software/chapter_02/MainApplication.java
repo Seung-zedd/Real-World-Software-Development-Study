3eba9ff6a5feff7e258959ca017b91f1fa1449b9
seung-je <csz0209@gmail.com>
2025-02-26 23:30:56 +0900

    chore: update comment to understand why interface can't create an instance

--- OOP/src/main/java/com/real_software/chapter_02/MainApplication.java
+++ OOP/src/main/java/com/real_software/chapter_02/MainApplication.java
@@ -6,6 +6,7 @@ public class MainApplication {
 
     public static void main(String[] args) throws IOException {
         final BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        //! 인터페이스는 추상 메서드의 집합체라 추상 클래스처럼 그 자체로는 인스턴스를 생성할 수 없다
         final BankStatementParser bankStatementParser = new BankStatementCSVParser();
 
         bankStatementAnalyzer.analyze(args[0], bankStatementParser);
